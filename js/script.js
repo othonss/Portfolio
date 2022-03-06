1ac8991834b84e69d8ffb804349ea5f724e50e17
othonss <othon.santos.silva@gmail.com>
2022-03-06 13:14:45 -0300

    Página projetos finalizada

--- js/script.js
+++ js/script.js
@@ -9,3 +9,45 @@ var spotifyContent = document.getElementById("spotify-content")
 moreInformationSpotify.addEventListener("click", function(){ 
     spotifyContent.classList.toggle("hide")
 })
+
+var moreInformationMosquito = document.getElementById("more-information-mosquito")
+var mosquitoContent = document.getElementById("mosquito-content")
+moreInformationMosquito.addEventListener("click", function(){ 
+    mosquitoContent.classList.toggle("hide")
+})
+
+var moreInformationWildbeast = document.getElementById("more-information-wildbeast")
+var wildbeastContent = document.getElementById("wildbeast-content")
+moreInformationWildbeast.addEventListener("click", function(){ 
+    wildbeastContent.classList.toggle("hide")
+})
+
+var moreInformationMuseu = document.getElementById("more-information-museu")
+var museuContent = document.getElementById("museu-content")
+moreInformationMuseu.addEventListener("click", function(){ 
+    museuContent.classList.toggle("hide")
+})
+
+var moreInformationNoticias = document.getElementById("more-information-noticias")
+var noticiasContent = document.getElementById("noticias-content")
+moreInformationNoticias.addEventListener("click", function(){ 
+    noticiasContent.classList.toggle("hide")
+})
+
+var moreInformationTarefas = document.getElementById("more-information-tarefas")
+var tarefasContent = document.getElementById("tarefas-content")
+moreInformationTarefas.addEventListener("click", function(){ 
+    tarefasContent.classList.toggle("hide")
+})
+
+var moreInformationImovi = document.getElementById("more-information-imovi")
+var imoviContent = document.getElementById("imovi-content")
+moreInformationImovi.addEventListener("click", function(){ 
+    imoviContent.classList.toggle("hide")
+})
+
+var moreInformationOthon = document.getElementById("more-information-othon")
+var othonContent = document.getElementById("othon-content")
+moreInformationOthon.addEventListener("click", function(){ 
+    othonContent.classList.toggle("hide")
+})
